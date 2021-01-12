81f1a9522aef2ded312c89074c4e0f9499d96328
purnchndr <purnchndr7@gmail.com>
2021-01-12 22:08:47 +0530

    Final commit

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -14,78 +14,92 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int scoreTeamA = 0, scoreTeamB = 0, countA = 0, countB =0;
+    //Initializing the variables that requared//
+    int scoreTeamA = 0, scoreTeamB = 0, countA = 0, countB = 0;
 
-    public void incementA3(View view){
+    //increment Score 3 for team A//
+    public void incementA3(View view) {
         scoreTeamA += 3;
         countA += 1;
         countDisplayA(countA);
         displayA(scoreTeamA);
     }
 
-    public void incementB3(View view){
+    //Increment Score 3 for team B//
+    public void incementB3(View view) {
         scoreTeamB += 3;
         countB += 1;
         countDisplayB(countB);
         displayB(scoreTeamB);
     }
 
-    public void incementA2(View view){
+    ////Increment Score 2 for team A//
+    public void incementA2(View view) {
         scoreTeamA += 2;
         countA += 1;
         countDisplayA(countA);
         displayA(scoreTeamA);
     }
 
-    public void incementB2(View view){
-        scoreTeamB += 2; countB += 1;
+    //Increment Score 2 for team B//
+    public void incementB2(View view) {
+        scoreTeamB += 2;
+        countB += 1;
         countDisplayB(countB);
         displayB(scoreTeamB);
     }
 
-    public void freeThrowA(View view){
+    //Increment Score 1 for team A//
+    public void freeThrowA(View view) {
         scoreTeamA += 1;
         countA += 1;
         countDisplayA(countA);
         displayA(scoreTeamA);
     }
 
-    public void freeThrowB(View view){
+    //Increment Score 1 for team B//
+    public void freeThrowB(View view) {
         scoreTeamB += 1;
         countB += 1;
         countDisplayB(countB);
         displayB(scoreTeamB);
     }
 
-  public  void reset(View view){
-        scoreTeamA =0;
+    //Reset all the values//
+    public void reset(View view) {
+        scoreTeamA = 0;
         scoreTeamB = 0;
-countA = 0;
-countB = 0;
-      countDisplayB(countB);
-      countDisplayA(countA);
-      displayA(scoreTeamA);
-      displayB(scoreTeamB);
-  }
-    private void displayA(int scoreA){
+        countA = 0;
+        countB = 0;
+        countDisplayB(countB);
+        countDisplayA(countA);
+        displayA(scoreTeamA);
+        displayB(scoreTeamB);
+    }
+
+//Update the Score for team A//
+    private void displayA(int scoreA) {
         TextView score = (TextView) findViewById(R.id.textViewA2);
         score.setText(String.valueOf(scoreA));
     }
 
-    private void displayB(int scoreB){
+//Update the score for team B//
+    private void displayB(int scoreB) {
         TextView score = (TextView) findViewById(R.id.textViewB2);
         score.setText(String.valueOf(scoreB));
     }
 
-    private void countDisplayA(int countA){
+//Update the goal count for team A//
+    private void countDisplayA(int countA) {
         TextView count = (TextView) findViewById(R.id.textViewA3);
         count.setText(String.valueOf(countA));
     }
 
-    private void countDisplayB(int countB){
+//Update the goal count for team B//
+    private void countDisplayB(int countB) {
         TextView count = (TextView) findViewById(R.id.textViewB3);
         count.setText(String.valueOf(countB));
     }
 
-
+//end of maim method //
 }
